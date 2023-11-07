ea9d169a00e43e83a7da6441491c02b80f1e3135
abhijithka <ab903733@dal.ca>
2023-11-07 10:13:21 -0400

    p4 ping and ip routing working, vla current address table fixing bug.

--- app/src/main/java/org/onosproject/ngsdn/tutorial/VlaComponent.java
+++ app/src/main/java/org/onosproject/ngsdn/tutorial/VlaComponent.java
@@ -263,8 +263,8 @@ public class VlaComponent {
             byte second_part = (byte) currentNum;
             int tempNum = (currentNum >> bitShift);
             byte first_part = (byte) tempNum;
-            byteNumbers[2*i] = second_part;
-            byteNumbers[(2*i) + 1] = first_part;
+            byteNumbers[2*i] = first_part;
+            byteNumbers[(2*i) + 1] = second_part;
         }
         log.info("Vla Address of first level {}, {} )...", byteNumbers[0], byteNumbers[1]);
 
@@ -498,11 +498,13 @@ public class VlaComponent {
         // ---- START SOLUTION ----
         String tableId = "IngressPipeImpl.current_vla_address_table";
 
+        byte[] octets = currentAddress.get();
+
         // Modify the field and action id to match your P4Info
         // ---- START SOLUTION ----
         PiCriterion match = PiCriterion.builder()
                 .matchExact(
-                        PiMatchFieldId.of("local_metadata.parser_local_metadata.destination_address_key"), currentAddress.get()
+                        PiMatchFieldId.of("local_metadata.parser_local_metadata.destination_address_key"), Arrays.copyOf(octets, octets.length)
                 )
                 .build();
 
