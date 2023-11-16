89be430df5865bcb1f7d930877b59ba8dc802105
abhijithka <ab903733@dal.ca>
2023-11-16 17:03:23 -0400

    ptf test cases added

--- VlaTests/NRTest.py
+++ VlaTests/NRTest.py
@@ -1,22 +1,15 @@
 
-from __future__ import print_function
-import os
-import sys
+
 
 from scapy.all import get_if_hwaddr
 
-sys.path.insert(0, os.path.join(os.getcwd(), 'lib'))
-
-from scapy.layers.inet6 import  _IPv6ExtHdr;
-from scapy.fields import FieldListField, PadField
 from scapy.sendrecv import srp
-import scapy.packet
-import scapy.utils
 from IPv6ExtHdrVLA import IPv6ExtHdrVLA
 from scapy.layers.inet6 import *
 from scapy.layers.l2 import Ether
 from scapy.pton_ntop import inet_pton, inet_ntop
 from scapy.utils6 import in6_getnsma, in6_getnsmac
+from NRUtils import resolveHostVlaAddress
 #from base_test import *
 
 
@@ -31,7 +24,7 @@ SWITCH1_MAC = "00:00:00:00:aa:01"
 SWITCH2_MAC = "00:00:00:00:aa:02"
 SWITCH3_MAC = "00:00:00:00:aa:03"
 HOST1_MAC = "00:00:00:00:00:01"
-HOST2_MAC = "00:00:00:00:00:02"
+HOST2_MAC = "00:00:00:00:00:1b"
 
 MAC_BROADCAST = "FF:FF:FF:FF:FF:FF"
 MAC_FULL_MASK = "FF:FF:FF:FF:FF:FF"
@@ -58,27 +51,6 @@ PACKET_IN_INGRESS_PORT_META_ID = 1
 
 
 
-
-
-def insert_vla_header(pkt, sid_list, current_level_param):
-    """Applies SRv6 insert transformation to the given packet.
-    """
-    # Set IPv6 dst to some valid IPV6 Address
-    pkt[IPv6].dst = HOST2_IPV6
-    # Insert VLA header between IPv6 header and payload
-    sid_len = len(sid_list)
-    srv6_hdr = IPv6ExtHdrVLA(
-        nh=pkt[IPv6].nh,
-        addresses=sid_list,
-        len=(sid_len * 2) - 1,
-        address_type = 0b01,
-        current_level = current_level_param,
-        number_of_levels= sid_len
-        )
-    pkt[IPv6].nh = 48  # next IPv6 header is VLA header
-    pkt[IPv6].payload = srv6_hdr / pkt[IPv6].payload
-    return pkt
-    
 
 def create_vla_current_address_entry(address_list, max_level_limit, level_size):
     result  = int("0", 2)
@@ -108,15 +80,10 @@ def getCommandLineArguments():
 
 def main():
 #  interface, vlaList, vlaCurrentLevel = getCommandLineArguments()
-   data = "HELLO WORLD"
+    data = "HELLO WORLD"
 #    interfaceMacAddress = get_if_hwaddr(interface)
-   vlaList = [4096,4096,4096,4096, 4096]
-   currentLevel = 2
-   packet = Ether(src="00:00:00:00:00:1a", dst="00:aa:00:00:00:01")/IPv6(src="::1", dst= "2002::2")/IPv6ExtHdrVLA()/UDP()/Raw(load=data)
-   packet = insert_vla_header(packet, vlaList, currentLevel)
-   print("data is ", data)
-   print("vla list  ", vlaList)
-   srp(packet, iface="h1a-eth0")   
+    responseStatus, responseMsg = resolveHostVlaAddress(HOST2_MAC, outInterface="h1a-eth0")
+    print(responseMsg)
 
 if __name__ == "__main__":
     main()
---
+++ VlaTests/NRUtils.py
@@ -0,0 +1,76 @@
+
+from scapy.layers.inet6 import *;
+from scapy.layers.inet6 import _ICMPv6NDGuessPayload, _ICMPv6
+from scapy.sendrecv import srp1
+
+class ICMPv6ND_NR(_ICMPv6NDGuessPayload, _ICMPv6, Packet):
+    name = "ICMPv6 Neighbor Discovery - Neighbor Solicitation"
+    fields_desc = [ByteEnumField("type", 137, icmp6types),
+                   ByteField("code", 0),
+                   XShortField("cksum", None),
+                   IntField("res", 0),
+                   IP6Field("tgt", "::")]
+    overload_fields = {IPv6: {"nh": 58, "dst": "ff02::1", "hlim": 255}}
+
+    def mysummary(self):
+        return self.sprintf("%name% (tgt: %tgt%)")
+
+    def hashret(self):
+        return bytes_encode(self.tgt) + self.payload.hashret()
+    
+class ICMPv6ND_NRReply(_ICMPv6NDGuessPayload, _ICMPv6, Packet):
+    NDP_FLAG_ROUTER    = 0x80000000
+    NDP_FLAG_NAME_RESOLUTION  = 0x10000000
+    name = "ICMPv6 Neighbor Discovery - Neighbor Solicitation"
+    fields_desc = [ByteEnumField("type", 138, icmp6types),
+                   ByteField("code", 0),
+                   XShortField("cksum", None),
+                   IntField("res", NDP_FLAG_ROUTER | NDP_FLAG_NAME_RESOLUTION),
+                   IP6Field("tgt", "::")]
+    overload_fields = {IPv6: {"nh": 58, "dst": "ff02::1", "hlim": 255}}
+
+    def mysummary(self):
+        return self.sprintf("%name% (tgt: %tgt%)")
+
+    def hashret(self):
+        return bytes_encode(self.tgt) + self.payload.hashret()
+    
+
+    
+class ICMPv6NDNROptSrcLLAddr(_ICMPv6NDGuessPayload, Packet):
+    name = "ICMPv6 Neighbor Discovery Option - Source Link-Layer Address"
+    NDP_TARGET_VLA_ADDR = 3
+    fields_desc = [ByteField("type", NDP_TARGET_VLA_ADDR),
+                   ByteField("len", 1),
+                   MACField("lladdr", ETHER_ANY)]
+
+    def mysummary(self):
+        return self.sprintf("%name% %lladdr%")
+    
+def genNdpNrPkt(target_host_mac):
+    NDP_NR_MAC = "33:33:30:00:00:00"
+    p = Ether(dst=NDP_NR_MAC) / IPv6(dst="::", src="::", hlim=255)
+    p /= ICMPv6ND_NR(tgt="::")
+    p /= ICMPv6NDNROptSrcLLAddr(lladdr=target_host_mac)
+    return p
+
+def resolveHostVlaAddress(hostId, outInterface):
+    ndp_nr_packet = genNdpNrPkt(hostId)
+    reply = srp1(ndp_nr_packet,outInterface)
+    replyMessage = ""
+    if reply:
+        if(Ether in reply and IPv6 in reply):
+            if reply[IPv6].nh == 58:
+                print("reply packet is ", reply)
+                ipPayload = ICMPv6ND_NRReply(reply[IPv6].payload)
+                print(ipPayload)
+                return (True,replyMessage)
+            else:
+                replyMessage =  "ICMP NDP NR not detected in reply"
+        else:
+            replyMessage = "Unexpected Response Type"
+    else:
+        replyMessage = "No response  from gateway."
+        return (False, replyMessage)
+
+    
