b36973b3c4ff08e47f8727da775ed2cec049a6f8
abhijithka <ab903733@dal.ca>
2023-11-17 18:40:11 -0400

    ping benchmark commit

--- VlaTests/NRUtils.py
+++ VlaTests/NRUtils.py
@@ -100,8 +100,7 @@ def genNdpNaPkt(target_ip, target_mac,
     return p
 
 def resolveHostVlaAddress(hostId, outInterface):
-    ndp_nr_packet = genNdpNsPkt(src_ip= "2001:1:1::a:ff", src_mac= "00:00:00:00:00:1a", target_ip="2001:1:2:0:0:0:0:ff",
-                                target_host_mac="00:00:00:00:00:1b")
+    ndp_nr_packet = genNdpNsPkt(src_ip= "2001:1:1::a:ff", src_mac= "00:00:00:00:00:1a", target_ip="2001:1:2:0:0:0:0:ff")
     print("packet is ", ndp_nr_packet)
     reply = srp(ndp_nr_packet,outInterface)
     replyMessage = ""
