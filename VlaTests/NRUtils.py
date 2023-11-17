b59b75eabf4c248bfa25f834a51ff0b6dfad9f19
abhijithka <ab903733@dal.ca>
2023-11-16 20:34:11 -0400

    ptf test cases added

--- VlaTests/NRUtils.py
+++ VlaTests/NRUtils.py
@@ -3,7 +3,7 @@ from scapy.layers.inet6 import *;
 from scapy.layers.inet6 import _ICMPv6NDGuessPayload, _ICMPv6
 from scapy.sendrecv import srp1, srp
 
-from base_test import NDP_NR_MAC
+from base_test import *
 
 
 IPV6_MCAST_MAC_1 = "33:33:00:00:00:01"
@@ -81,6 +81,7 @@ def genNdpNrPkt(target_host_mac):
 
 
 def genNdpNsPkt(target_ip, src_mac=HOST1_MAC, src_ip=HOST1_IPV6):
+    NDP_NR_MAC = "33:33:00:00:00:01"
     nsma = in6_getnsma(inet_pton(socket.AF_INET6, target_ip))
     d = inet_ntop(socket.AF_INET6, nsma)
     dm = in6_getnsmac(nsma)
