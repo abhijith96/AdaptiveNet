9f9ccc91be820452c83a0e501ae0b53507aeba30
abhijithka <ab903733@dal.ca>
2023-11-18 16:50:25 -0400

    ping benchmark commit

--- VlaTests/VlaPing.py
+++ VlaTests/VlaPing.py
@@ -9,9 +9,6 @@ from Utils import createVlaPacket, getMacAddress
 import time
 from scapy.all import conf
 from NRUtils import resolveHostVlaAddress, getCurrentHostVlaAddress, getDefaultMacAddress, getDefaultInterface
-import subprocess
-import os
-import signal
 import time
 
 def test():
@@ -92,16 +89,6 @@ def vla_ping(targetHostId):
         replyMessage = "No response from ping."
         return (False, replyMessage, rtt)
 
-def run_python_file_in_namespace(namespace_name, python_file_path):
-    try:
-        # Use nsenter to enter the network namespace and run the Python file
-        nsenter_command = ["nsenter", "--net --mount --ipc --pid --uts", "--target", namespace_name, "python", python_file_path]
-        process = subprocess.Popen(nsenter_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return process
-
-    except subprocess.CalledProcessError as e:
-            print(f"Error: {e}")
-            return None
     
 def main():
     targetHost = "00:00:00:00:00:1b"
@@ -112,36 +99,6 @@ def main():
     (pingStatus,replyMessage, rtt) = vla_ping(targetHost)
     print(replyMessage)
     print("Round Trip Time is  {:.3f} ".format(rtt*1000))
-
-    namespace_name_1 = "10"
-    namespace_name_2 = "12"
-    python_file_path_2 = "/home/VlaTests/VlaPing.py"
-    python_file_path_1 = "/home/VlaTests/VlaPingListener.py"
-
-    # Run the first Python file in the first namespace
-    process_1 = run_python_file_in_namespace(namespace_name_1, python_file_path_1)
-
-    # Wait for a moment to ensure the first file is running
-    time.sleep(5)
-
-    # Run the second Python file in the second namespace
-    process_2 = run_python_file_in_namespace(namespace_name_2, python_file_path_2)
-
-    # Wait for the second file to finish and capture its output
-    output, errors = process_2.communicate()
-
-    # Terminate the first file when the second file ends
-    process_1.terminate()
-
-    # Optionally wait for the first file to terminate gracefully
-    process_1.wait()
-
-    print("Output of the second file:")
-    print(output.decode('utf-8'))
-    print("Errors of the second file:")
-    print(errors.decode('utf-8'))
-    print("Both files completed.")
-
 
 if __name__ == "__main__":
     main()
