9bac3d8d0e061c9e863c49d20aecd12f76be8753
abhijithka <ab903733@dal.ca>
2023-11-18 15:48:32 -0400

    ping benchmark commit

--- VlaTests/Runner.py
+++ VlaTests/Runner.py
@@ -3,17 +3,16 @@ import subprocess
 def getNetworkNamespaces():
     try:
         # Run the shell command and capture the output
-        result = subprocess.check_output("lsns --type=net", shell=True, universal_newlines=True)
+        result = subprocess.check_output("lsns --type=net", shell=True)
 
         # Split the output into lines and return as a list
         return result.strip().split('\n')
     except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
+        print("Error: %s ".format(e))
         return []
 
-# Example: List files in the current directory
-command = "ls"
-output_lines = getNetworkNamespaces(command)
+
+output_lines = getNetworkNamespaces()
 
 # Print the result
 print("Output:")
