b788f82ff694450a2fc0f2e9e4c51f5b713ee554
abhijithka <ab903733@dal.ca>
2023-11-18 12:57:18 -0400

    ping benchmark commit

--- VlaTests/VlaPing.py
+++ VlaTests/VlaPing.py
@@ -76,7 +76,7 @@ def ping(targetHostId):
                 ipPayload = IPv6ExtHdrVLA(reply[IPv6].payload)
                 if ipPayload[UDP] and ipPayload[UDP].sport == 50001:
                     replyMessage = "Ping  successful! " + ipPayload[Raw].load
-                    rtt = end_time - packet.sent_time
+                    rtt = end_time - start_time
                     return (True,replyMessage, rtt)
                 else:
                     replyMessage = "Ping Failed UDP not found or UDP src port does not match "
