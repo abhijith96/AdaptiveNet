c11e85012d37ff3d98db061f8ece1459fa6bb583
abhijithka <ab903733@dal.ca>
2023-11-20 12:05:34 -0400

    chaning main and p4 big change

--- VlaTests/IpPingReply.py
+++ VlaTests/IpPingReply.py
@@ -2,32 +2,31 @@ from scapy.all import sr1,sendp, sniff, send, Raw
 from scapy.layers.inet6 import UDP, IPv6
 from scapy.layers.l2 import Ether
 from Utils import createIpPingReplyPacket, IP_PING_D_PORT
-from NRUtils import getDefaultInterface
+from NRUtils import getDefaultInterface, getDefaultMacAddress
 
 
 
 def custom_packet_filter(packet):
-    # Check if the packet is an Ethernet frame
     if Ether not in packet:
         return False
-    # Specify the desired destination MAC address
-    if IPv6 in packet:
-        if(UDP in packet):
-            destination_port = packet[UDP].dport
-            if(destination_port == IP_PING_D_PORT):
-                return True
-            else:
-                print(packet.show())
-                print("Invalid UDP port")
+    if(packet[Ether].dst == macAddress):
+        if IPv6 in packet:
+            if(UDP in packet):
+                destination_port = packet[UDP].dport
+                if(destination_port == IP_PING_D_PORT):
+                    return True
+                else:
+                    print("Invalid UDP port")
     return False
 interface = ""
+macAddress = ""
 
 def process_udp_packet(packet):
     if IPv6 in packet and UDP in packet:
         reply = "Ping Reply"
         # print("received packet is ", packet)
         modified_packet = createIpPingReplyPacket(packet, reply)
-        # print("modified packet is ", modified_packet)
+        print("modified packet is ", modified_packet)
         sendp(modified_packet, iface=interface)  
     else:
         print("UnRecognized packet")
@@ -37,6 +36,8 @@ def process_udp_packet(packet):
 def pingListener(interfaceName):
     global interface
     interface = interfaceName
+    global macAddress
+    macAddress = getDefaultMacAddress()
     sniff(prn=process_udp_packet, lfilter=custom_packet_filter)
     return None
 
