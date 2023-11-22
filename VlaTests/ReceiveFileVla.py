bd01bae26babb0b447dd9a7de0ad0dba6d565945
abhijithka <ab903733@dal.ca>
2023-11-22 11:45:56 -0400

    adding file sending test case

--- VlaTests/ReceiveFileVla.py
+++ VlaTests/ReceiveFileVla.py
@@ -1,9 +1,26 @@
 import Utils
 import NRUtils
-
-
+from scapy.layers.l2 import Ether
+from scapy.layers.inet6 import IPv6, UDP
 import socket
 from scapy.all import *
+from IPv6ExtHdrVLA import IPv6ExtHdrVLA
+
+def custom_packet_filter(packet):
+    if Ether not in packet:
+        return False
+    # Specify the desired destination MAC address
+    if IPv6 in packet and packet[IPv6].nh == 48:
+        ipPayload = IPv6ExtHdrVLA(packet[Raw].load)
+        #print("ip payload is ", ipPayload)
+        if(UDP in ipPayload):
+            destination_port = ipPayload[UDP].dport
+            if(destination_port == Utils.VLA_FILE_TRANSFER_D_PORT):
+                return True
+
+
+    # Check if the destination MAC address matches the desired value
+    return False
 
 def receive_file(output_file_path, listening_port):
     packets = sniff(filter=f'udp and port {listening_port}', count=0, iface=NRUtils.getDefaultInterface())
@@ -16,7 +33,5 @@ def receive_file(output_file_path, listening_port):
 
 if __name__ == "__main__":
     output_file_path = "output_file.txt"
-    listening_ip = "0.0.0.0"  # Listen on all available interfaces
-    listening_port = 12345  # Choose a port to listen on
-
+    # Choose a port to listen on
     receive_file(output_file_path, Utils.VLA_FILE_TRANSFER_D_PORT)
