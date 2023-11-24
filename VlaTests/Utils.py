0401f6a650f6e85fed573872e4cda161e7205c7a
abhijithka <ab903733@dal.ca>
2023-11-24 11:49:36 -0400

    Ip ping test bug fix

--- VlaTests/Utils.py
+++ VlaTests/Utils.py
@@ -154,6 +154,6 @@ def createIpPingReplyPacket(requestPacket, replyMsg):
 
     #pkt = Ether(src=eth_src, dst=eth_dst)/IPv6(src=ip_src, dst = ip_dst)/UDP(sport= udp_sport, dport = udp_dport)/Raw(load=replyMsg)
     # pkt = IPv6(dst = ip_dst)/ICMPv6EchoReply()/Raw(load=replyMsg)
-    pkt = Ether(src=eth_src, dst=eth_dst)/IPv6(src=ip_src, dst = ip_dst)/ICMPv6EchoReply()
+    pkt = Ether(src=eth_src, dst=eth_dst)/IPv6(src=ip_src, dst = ip_dst)/ICMPv6EchoReply()/Raw(load=replyMsg)
     return pkt
 
